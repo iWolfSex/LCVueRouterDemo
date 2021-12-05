4ff24d6b3e8e1d9527cb4f0ede43ea88b5601792
iiuchao <727388890@qq.com>
2021-12-05 18:34:03 +0800

    多级路由

---
+++ vue_router_demo/1-src-多级路由/pages/News.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <ul>
+      <li>news0001</li>
+      <li>news0001</li>
+      <li>news0002</li>
+      <li>news0003</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "News",
+};
+</script>
+
+<style>
+</style>
---
+++ vue_router_demo/src/router/index.js
@@ -0,0 +1,31 @@
+//该文件专门用于穿件整个应用的的 路由器
+import VueRouter from "vue-router";
+
+//引入组件
+import About from '../pages/About'
+import Home from '../pages/Home'
+import "bootstrap/dist/css/bootstrap.min.css"
+import News from '../pages/News'
+import Message from '../pages/Message'
+//c创建并且暴露路由
+export default new VueRouter({
+    routes: [
+        {
+            path: '/about',
+            component: About
+        }, {
+            path: '/home',
+            component: Home,
+            children: [
+                {
+                    path: 'news',
+                    component: News,
+                },
+                {
+                    path: 'message',
+                    component: Message,
+                }
+            ]
+        }
+    ]
+})
